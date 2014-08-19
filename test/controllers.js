3a4c21e00bb7f495e55852395bedae17269eabf0
Yuri <nscapex@gmail.com>
2014-08-19 10:13:11 -0400

    readme

--- test/controllers.js
+++ test/controllers.js
@@ -9,33 +9,33 @@ var WagerCtrl = require('../lib/controllers/wager')
 
 describe('controller unit tests', function(){
 
-	var req = {}
-		, res = {}
-		, next = {}
+  var req = {}
+    , res = {}
+    , next = {}
 
-	//sync db
-	before(function(done){
-		this.timeout(10000);
-		sequelize
-		.sequelize
-		.sync({force:true})
-		.complete(function(){
-			done();
-		})
-	})
+  //sync db
+  before(function(done){
+    this.timeout(10000);
+    sequelize
+    .sequelize
+    .sync({force:true})
+    .complete(function(){
+      done();
+    })
+  })
 
-	describe('auth controller', function(){
-		describe('#register()', function(){
-			
-			beforeEach(function(){
-				req.body = {
-					username: 'testuser',
-					password: 'testpassword',
-					email: 'test@test.test'
-				}
-			});
+  describe('auth controller', function(){
+    describe('#register()', function(){
+      
+      beforeEach(function(){
+        req.body = {
+          username: 'testuser',
+          password: 'testpassword',
+          email: 'test@test.test'
+        }
+      });
 
-			it('should return a 200 since its a new user', function(done) {
+      it('should return a 200 since its a new user', function(done) {
 
         res.json = res.send = function(httpStatus) {
           expect(httpStatus).to.equal(200);
@@ -45,7 +45,7 @@ describe('controller unit tests', function(){
         AuthCtrl.register(req, res, next);
       });
 
-			it('should return a 403 since user already exists', function(done) {
+      it('should return a 403 since user already exists', function(done) {
 
         res.json = res.send = function(httpStatus) {
           expect(httpStatus).to.equal(403);
@@ -54,19 +54,19 @@ describe('controller unit tests', function(){
 
         AuthCtrl.register(req, res, next);
       });
-		})
+    })
 
-		describe('#registerModerator()', function(){
-			
-			beforeEach(function(){
-				req.body = {
-					username: 'testmod',
-					password: 'testmod',
-					email: 'mod@test.test'
-				}
-			});
+    describe('#registerModerator()', function(){
+      
+      beforeEach(function(){
+        req.body = {
+          username: 'testmod',
+          password: 'testmod',
+          email: 'mod@test.test'
+        }
+      });
 
-			it('should return a 200 since its a new user', function(done) {
+      it('should return a 200 since its a new user', function(done) {
 
         res.json = res.send = function(httpStatus) {
           expect(httpStatus).to.equal(200);
@@ -76,7 +76,7 @@ describe('controller unit tests', function(){
         AuthCtrl.registerModerator(req, res, next);
       });
 
-			it('should return a 403 since user already exists', function(done) {
+      it('should return a 403 since user already exists', function(done) {
 
         res.json = res.send = function(httpStatus) {
           expect(httpStatus).to.equal(403);
@@ -85,34 +85,34 @@ describe('controller unit tests', function(){
 
         AuthCtrl.registerModerator(req, res, next);
       });
-		})
-	})
+    })
+  })
 
-	describe('category controller', function(){
-		describe('#create()', function(){
-			beforeEach(function(){
-				req.body = {
-					name: 'League of Legends'
-				}
-			});
+  describe('category controller', function(){
+    describe('#create()', function(){
+      beforeEach(function(){
+        req.body = {
+          name: 'League of Legends'
+        }
+      });
 
-			it('should return a 200', function(done) {
+      it('should return a 200', function(done) {
         res.json = res.send = function(httpStatus) {
           expect(httpStatus).to.equal(200);
           done();
         };
         CategoryCtrl.create(req, res, next);
       });
-		})
+    })
 
-		describe('#findByName()', function(){
-			beforeEach(function(){
-				req.body = {
-					name: 'League of Legends'
-				}
-			});
+    describe('#findByName()', function(){
+      beforeEach(function(){
+        req.body = {
+          name: 'League of Legends'
+        }
+      });
 
-			it('should return a 200', function(done) {
+      it('should return a 200', function(done) {
         res.json = res.send = function(httpStatus) {
           expect(httpStatus).to.equal(200);
           done();
@@ -120,13 +120,13 @@ describe('controller unit tests', function(){
         CategoryCtrl.findByName(req, res, next);
       });
 
-		})
-	})
+    })
+  })
 
-	describe('participant controller', function(){
-		describe('#create()', function(){
+  describe('participant controller', function(){
+    describe('#create()', function(){
 
-			it('should return a 200, with the first participants data', function(done) {
+      it('should return a 200, with the first participants data', function(done) {
         res.json = res.send = function(httpStatus, data) {
           expect(httpStatus).to.equal(200);
           expect(data.name).to.equal('Counter Logic Gaming');
@@ -145,30 +145,30 @@ describe('controller unit tests', function(){
         req = {body: {name: 'Team Solo Mid', tag: 'TSM'}}
         ParticipantCtrl.create(req, res, next);
       });
-		})
-	})
+    })
+  })
 
-	describe('event controller', function(){
-		describe('#create()', function(){
-			var tCategory;
-			it('should return a 200, with the category', function(done) {
+  describe('event controller', function(){
+    describe('#create()', function(){
+      var tCategory;
+      it('should return a 200, with the category', function(done) {
         req.body = {name: 'League of Legends'}
         res.json = res.send = function(httpStatus, data) {
-        	tCategory = data;
+          tCategory = data;
           expect(httpStatus).to.equal(200);
           done();
         };
         CategoryCtrl.findByName(req, res, next);
       });
 
-			it('should return a 200, with event data', function(done) {
+      it('should return a 200, with event data', function(done) {
         req.body = {
-        	title: 'Counter Logic Gaming VS TSM', 
-        	beginsAt: new Date(),
-        	UserId: 2,
-        	CategoryId: tCategory.id
-      	}
-      	res.json = res.send = function(httpStatus, data) {
+          title: 'Counter Logic Gaming VS TSM', 
+          beginsAt: new Date(),
+          UserId: 2,
+          CategoryId: tCategory.id
+        }
+        res.json = res.send = function(httpStatus, data) {
           expect(httpStatus).to.equal(200);
           expect(data.title).to.equal('Counter Logic Gaming VS TSM');
           expect(data.CategoryId).to.equal(tCategory.id);
@@ -177,17 +177,17 @@ describe('controller unit tests', function(){
         
         EventCtrl.create(req, res, next);
       });
-		})
-	})
+    })
+  })
 
-	describe('eventparticipant controller', function(){
-		describe('#create()', function(){
-			var tEventParticipant1
-			it('should return a 200, with the first eventparticipant object', function(done) {
+  describe('eventparticipant controller', function(){
+    describe('#create()', function(){
+      var tEventParticipant1
+      it('should return a 200, with the first eventparticipant object', function(done) {
         req.body = {
-        	ParticipantId: 1,
-        	EventId: 1,
-        	pocket: 0.5
+          ParticipantId: 1,
+          EventId: 1,
+          pocket: 0.5
         }
         res.json = res.send = function(httpStatus, data) {
           expect(httpStatus).to.equal(200);
@@ -197,10 +197,10 @@ describe('controller unit tests', function(){
         EventParticipantCtrl.create(req, res, next);
       });
 
-			it('should return a 200, with the eventparticipant1 object and the associated participant/event', function(done) {
+      it('should return a 200, with the eventparticipant1 object and the associated participant/event', function(done) {
         req.body = {
-        	EventId: tEventParticipant1.EventId,
-        	ParticipantId: tEventParticipant1.ParticipantId
+          EventId: tEventParticipant1.EventId,
+          ParticipantId: tEventParticipant1.ParticipantId
         }
         res.json = res.send = function(httpStatus, data) {
           expect(httpStatus).to.equal(200);
@@ -212,11 +212,11 @@ describe('controller unit tests', function(){
       });
 
       var tEventParticipant2
-			it('should return a 200, with the second eventparticipant object', function(done) {
+      it('should return a 200, with the second eventparticipant object', function(done) {
         req.body = {
-        	ParticipantId: 2,
-        	EventId: 1,
-        	pocket: 1
+          ParticipantId: 2,
+          EventId: 1,
+          pocket: 1
         }
         res.json = res.send = function(httpStatus, data) {
           expect(httpStatus).to.equal(200);
@@ -228,8 +228,8 @@ describe('controller unit tests', function(){
 
       it('should return a 200, with the eventparticipant2 object and the associated participant/event', function(done) {
         req.body = {
-        	EventId: tEventParticipant2.EventId,
-        	ParticipantId: tEventParticipant2.ParticipantId
+          EventId: tEventParticipant2.EventId,
+          ParticipantId: tEventParticipant2.ParticipantId
         }
         res.json = res.send = function(httpStatus, data) {
           expect(httpStatus).to.equal(200);
@@ -239,18 +239,18 @@ describe('controller unit tests', function(){
         };
         EventParticipantCtrl.findById(req, res, next);
       });
-			
-		})
-	})
+      
+    })
+  })
 
 describe('wager controller', function(){
-		describe('#findById()', function(){
+    describe('#findById()', function(){
 
-			it('should return a 200, placing a bet', function(done) {
+      it('should return a 200, placing a bet', function(done) {
         req.body = { 
-        	amount: 10,
-        	payout: 8,
-        	EventParticipantId: 1
+          amount: 10,
+          payout: 8,
+          EventParticipantId: 1
         }
         res.json = res.send = function(httpStatus, data) {
           expect(httpStatus).to.equal(200);
@@ -262,9 +262,9 @@ describe('wager controller', function(){
 
       it('should return a 200, placing a bet', function(done) {
         req.body = { 
-        	amount: 10,
-        	payout: 8,
-        	EventParticipantId: 1
+          amount: 10,
+          payout: 8,
+          EventParticipantId: 1
         }
         res.json = res.send = function(httpStatus, data) {
           expect(httpStatus).to.equal(200);
@@ -273,15 +273,15 @@ describe('wager controller', function(){
         };
         WagerCtrl.create(req, res, next);
       });
-		})
+    })
 
-		describe('#findById()', function(){
+    describe('#findById()', function(){
 
-			it('should return a 200, placing a bet', function(done) {
+      it('should return a 200, placing a bet', function(done) {
         req.body = { 
-        	amount: 9,
-        	payout: 9,
-        	EventParticipantId: 2
+          amount: 9,
+          payout: 9,
+          EventParticipantId: 2
         }
         res.json = res.send = function(httpStatus, data) {
           expect(httpStatus).to.equal(200);
@@ -291,11 +291,11 @@ describe('wager controller', function(){
         WagerCtrl.create(req, res, next);
       });
 
-			it('should return a 200, placing a bet', function(done) {
+      it('should return a 200, placing a bet', function(done) {
         req.body = { 
-        	amount: 9,
-        	payout: 9,
-        	EventParticipantId: 2
+          amount: 9,
+          payout: 9,
+          EventParticipantId: 2
         }
         res.json = res.send = function(httpStatus, data) {
           expect(httpStatus).to.equal(200);
@@ -304,15 +304,15 @@ describe('wager controller', function(){
         };
         WagerCtrl.create(req, res, next);
       });
-		})
-	})
+    })
+  })
 
-	describe('event controller', function(){
-		describe('#findById()', function(){
+  describe('event controller', function(){
+    describe('#findById()', function(){
 
-			it('should return a 200', function(done) {
+      it('should return a 200', function(done) {
         req.body = {
-        	EventId: 1
+          EventId: 1
         }
         res.json = res.send = function(httpStatus, data) {
           expect(httpStatus).to.equal(200);
@@ -320,7 +320,7 @@ describe('wager controller', function(){
         };
         EventCtrl.getWithOdds(req, res, next);
       });
-		})
-	})
+    })
+  })
 })
 
